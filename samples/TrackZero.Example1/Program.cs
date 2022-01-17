a6af263ca1a5a150aa91246127a1b8b87834cfac
Adel Alzubeir <adel@leira.io>
2022-01-17 13:02:04 +0300

    Adds iframe embedding capabilities

--- samples/TrackZero.Example1/Program.cs
+++ samples/TrackZero.Example1/Program.cs
@@ -25,19 +25,22 @@ namespace TrackZero.Example1
             string analyticsSpaceId = "7766";
             await trackZeroClient.CreateAnalyticsSpaceAsync(analyticsSpaceId);
 
-            // Prepare your entity (Order object), if we already have an entity of Type "Order" and Id 1, this will update the information stored in TrackZero with the ones we set here.
+            // Prepare your entity (Order object), if we already have an entity of Type "Order" and Id 1,
+            // this will update the information stored in TrackZero with the ones we set here.
             Entity order = new Entity("Orders", 1)
                 // Adding attributes (Properties) to send to TrackZero and make it reportable.
                 .AddAttribute("Quantity", 99.0)
                 .AddAttribute("Total Amount", 99.0)
                 .AddAttribute("Time", DateTimeOffset.UtcNow)
                 // Adding Reference Attributes that will link to other entities.
-                // Since the Entity of Type "Product" and Id 758 doesn't exist in our project, it will be created automatically. We can add more attributes to it later.
+                // Since the Entity of Type "Product" and Id 758 doesn't exist in our project, it will be created automatically.
+                // We can add more attributes to it later.
                 .AddEntityReferencedAttribute("Items", "Products", 758)
                 .AddEntityReferencedAttribute("Items", "Products", 214)
                 // Adding reference to the user who made the order
                 .AddEntityReferencedAttribute("Order By", "Users", 245)
-                // To link this order to a country, you can simple pass the lat and long of the order and TrackZero will automatically manage this data appropriately to allow Map Charts.
+                // To link this order to a country, you can simple pass the lat and long of the order and
+                // TrackZero will automatically manage this data appropriately to allow Map Charts.
                 .AddAutomaticallyTranslatedGeoPoint(41.037086118695825, 28.98489855136287);
 
             // Send the Entity to TrackZero
@@ -48,10 +51,13 @@ namespace TrackZero.Example1
            
             // In order for your customer to run analytics on the data, we need to create a session key then redirect them to the TrackZero Spaces portal.
             // We will need to specify the custoemr space id and the duration of the session. In this case the duration is 30 minutes.
-            // the session duration cannot be less than 5 minutes or longer than 60 minutes.
+            // the session duration cannot be less than 5 minutes or longer than 14400 minutes (4 hours).
             var session = await trackZeroClient.CreateAnalyticsSpacePortalSessionAsync(analyticsSpaceId, TimeSpan.FromMinutes(30));
 
             // redirect the user to the url stored in session.OperationData.Url
+            // OR
+            // Embed the Dashboards (session.OperationData.EmbeddedDashboarsdUrl) and Reports (session.OperationData.EmbeddedReportsUrl)
+            // in an iframe tag in your pages so the user doesn't leave your site.
             // code omitted as this is a frontend practice.
 
             // delete the Entity "Order" with id 1 from TrackZero.
--- src/TrackZero/DataTransfer/AnalyticsSpacePortalSession.cs
+++ src/TrackZero/DataTransfer/AnalyticsSpacePortalSession.cs
@@ -6,14 +6,24 @@
     public class AnalyticsSpacePortalSession
     {
         /// <summary>
-        /// 
+        /// A session key used for authorization, there is no direct use of this key.
         /// </summary>
         public string SessionKey { get; set; }
 
         /// <summary>
-        /// 
+        /// The URL to redirect the user to. This URL will allow the user to access the specific Analytics Space Session for the specified TTL in the request.
         /// </summary>
         public string Url { get; set; }
+
+        /// <summary>
+        /// The URL to use in <IFrame /> tag if you plan on embedding TrackZero Dashboards page inside your web page. This URL is scoped to the specified  Analytics Space Session for the specified TTL in the request.
+        /// </summary>
+        public string EmbeddedDashboarsdUrl { get; set; }
+
+        /// <summary>
+        /// The URL to use in <IFrame /> tag if you plan on embedding TrackZero Reports page inside your web page. This URL is scoped to the specified  Analytics Space Session for the specified TTL in the request.
+        /// </summary>
+        public string EmbeddedReportsUrl { get; set; }
     }
 
 }
